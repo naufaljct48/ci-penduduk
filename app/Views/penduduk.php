9dca33997cd7dab7991cee7732d4b965964e9e63
Naufal Aziz Albaaqie <naufaljct48@gmail.com>
2022-10-20 20:08:26 +0700

    -

--- app/Views/penduduk.php
+++ app/Views/penduduk.php
@@ -30,7 +30,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Hapus Data</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
         <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true"></span>
         </button>
@@ -115,8 +115,8 @@
                 {data: 'tahun',name: 'tahun'},
                 {
                     data: function(row) {
-                        let html = '<button class="btn btn-warning btn-sm mr-1" data-toggle="modal" data-target="#editData" data-id="' + row.id + '" data-penduduk="' + row.penduduk + '" data-bulan="' + row.bulan + '" data-tahun="' + row.tahun + '"><i class="fas fa-edit"></i></button>';
-                        html += '<button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapusData" data-id="' + row.id + '"><i class="fa fa-trash"></i></button>'
+                        let html = '<button class="btn btn-warning btn-sm mr-1" data-toggle="modal" data-target="#editData" data-id="' + row.id + '" data-penduduk="' + row.penduduk + '" data-bulan="' + row.bulan + '" data-tahun="' + row.tahun + '"><i class="fas fa-edit"></i></button> <div class="modal fade" id="editData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Edit Data</h5><button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"></span></button></div><div class="modal-body"><div class="form-group row"><div class="col-sm-5 col-form-label"><label for="penduduk">Jumlah Penduduk</label></div><div class="col-sm-7"><input type="number" class="form-control" name="penduduk" id="penduduk" placeholder="Jumlah Penduduk" value="' + row.penduduk + '" ><input type="hidden" name="id" id="' + row.id + '"></div></div><div class="form-group row"><div class="col-sm-5 col-form-label"><label for="bulan">Bulan</label></div><div class="col-sm-7"><select name="bulan" id="bulan" class="form-control"><option value="1">Januari</option><option value="2">Februari</option><option value="3">Maret</option><option value="4">April</option><option value="5">Mei</option><option value="6">Juni</option><option value="7">Juli</option><option value="8">Agustus</option><option value="9">September</option><option value="10">Oktober</option><option value="11">November</option><option value="12">Desember</option></select></div></div><div class="form-group row"><div class="col-sm-5 col-form-label"><label for="tahun">Tahun</label></div><div class="col-sm-7"><select name="tahun" id="tahun" class="form-control"><option value="2019">2019</option><option value="2020">2020</option><option value="2021">2021</option><option value="2022">2022</option><option value="2023">2023</option><option value="2024">2024</option><option value="2025">2025</option><option value="2026">2026</option><option value="2027">2027</option><option value="2028">2028</option><option value="2029">2029</option></select></div></div></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button><button onclick="edit();" class="btn btn-primary">Simpan</button></div></div></div></div>';
+                        html += ' <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapusData" data-id="' + row.id + '"><i class="fa fa-trash"></i></button>'
                         return html;
                     }
                 }
@@ -149,7 +149,28 @@
             type    : 'POST',
             dataType: 'html',
             success : function(result){
-
+                $('#tablePenduduk').DataTable().ajax.reload();
+                $('#tambahPenduduk').modal('hide');
+    hasil();
+    finish();
+        $("#boxResult").html(result);
+        }
+        });
+    }
+    function edit(){
+    start();
+    post();
+        var penduduk = $('#penduduk').val();
+        var bulan = $('#bulan').val();
+        var tahun = $('#tahun').val();
+        $.ajax({
+            url : '<?= site_url('penduduk/ubah') ?>',
+            data    : 'penduduk='+penduduk+'&bulan='+bulan+'&tahun='+tahun,
+            type    : 'POST',
+            dataType: 'html',
+            success : function(result){
+                $('#tablePenduduk').DataTable().ajax.reload();
+                $('#tambahPenduduk').modal('hide');
     hasil();
     finish();
         $("#boxResult").html(result);
@@ -157,69 +178,4 @@
         });
     }
 </script>
-<!-- Modal Edit -->
-<div class="modal fade" id="editData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
-        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="form-group row">
-                    <div class="col-sm-5 col-form-label">
-                        <label for="penduduk">Jumlah Penduduk</label>
-                    </div>
-                    <div class="col-sm-7">
-                        <input type="number" class="form-control" name="penduduk" id="penduduk" placeholder="Jumlah Penduduk" value="">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-5 col-form-label">
-                        <label for="bulan">Bulan</label>
-                    </div>
-                    <div class="col-sm-7">
-                        <select name="bulan" id="bulan" class="form-control">
-                            <option value="1">Januari</option>
-                            <option value="2">Februari</option>
-                            <option value="3">Maret</option>
-                            <option value="4">April</option>
-                            <option value="5">Mei</option>
-                            <option value="6">Juni</option>
-                            <option value="7">Juli</option>
-                            <option value="8">Agustus</option>
-                            <option value="9">September</option>
-                            <option value="10">Oktober</option>
-                            <option value="11">November</option>
-                            <option value="12">Desember</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-5 col-form-label">
-                        <label for="tahun">Tahun</label>
-                    </div>
-                    <div class="col-sm-7">
-                        <select name="tahun" id="tahun" class="form-control">
-                          <?php
-                            $firstYear = (int)date('Y') - 3;
-                            $lastYear = $firstYear + 10;
-                            for($i=$firstYear;$i<=$lastYear;$i++)
-                            {
-                                echo '<option value='.$i.'>'.$i.'</option>';
-                            }
-                          ?>
-                        </select>
-                    </div>
-                </div>
-                </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
-        <button onclick="kirim();" class="btn btn-primary">Simpan</button>
-      </div>
-    </div>
-  </div>
-</div>
 <?= $this->endSection(); ?>
